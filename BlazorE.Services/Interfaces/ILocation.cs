897709797790087269a67162bb0c23cbef27a181
A-Finger <anthony.palchyk@gmail.com>
2022-08-30 16:21:41 +0300

    Changed: Minor changes

--- BlazorE.Services/Interfaces/ILocation.cs
+++ BlazorE.Services/Interfaces/ILocation.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BlazorE.Models.Locations;
+﻿using BlazorE.Models.Locations;
 
 namespace BlazorE.Services.Interfaces
 {
